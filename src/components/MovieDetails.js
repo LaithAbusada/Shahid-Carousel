382cf7b31a444f4455ed3898b5fbde6820790103
Laith Abusada <lai20200812@std.psut.edu.jo>
2024-05-16 13:38:12 +0300

    Added unsaved changes from local repository

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./MovieDetails.css";
 function MovieDetails({ movie, genres }) {
   return (
-    <div className="movie-info">
-      <h4>{movie.item.title}</h4>
-      {genres && genres.length > 0 && (
-        <ul className="horizontal-list">
-          {genres.map((genre, index) => (
-            <li key={index}>{genre.title}</li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <div className="movie-info">
+        <h4>{movie.item.title}</h4>
+        {genres && genres.length > 0 && (
+          <ul className="horizontal-list">
+            {genres.map((genre, index) => (
+              <li key={index}>{genre.title}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 }
 
